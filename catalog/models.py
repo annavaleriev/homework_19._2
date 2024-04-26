e4da15d5d67e48b5334ecf776e3bc6a806c2b9ce
annavaleriev <filenko.a@gmail.com>
2024-04-26 19:25:37 +0300

    add

--- catalog/models.py
+++ catalog/models.py
@@ -21,9 +21,17 @@ class Product(models.Model):
     image = models.ImageField(
         upload_to="product_image/", verbose_name="Изображение (превью)", **NULLABLE
     )
+    # category = models.ForeignKey(
+    #     Category, on_delete=models.CASCADE, verbose_name="категория", **NULLABLE
+    # )
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, verbose_name="категория"
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="категория",
+        **NULLABLE,
+        related_name="категории",
     )
+
     price = models.DecimalField(
         max_digits=10, decimal_places=2, verbose_name="Цена за покупку"
     )
