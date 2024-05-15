2e77f7c3e5372147925f4d9679536e4c2989bf93
annavaleriev <filenko.a@gmail.com>
2024-05-15 15:31:31 +0300

    Создано новое приложение Blog

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import home, ProductDetailView, ContactsTemplateView
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("contacts/", ContactsTemplateView.as_view(), name="contacts_list"),
+    path("products/<int:pk>", ProductDetailView.as_view(), name="product_info"), # Добавление URL с параметром
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Blog
+
+
+class BlogListView(ListView):   # создаем класс BlogListView, который наследуется от ListView
+    model = Blog # указываем модель, с которой будет работать наш класс
+
+    def get_queryset(self, *args, **kwargs): # тут мы переопределяем метод get_queryset
+        queryset = super().get_queryset().order_by(*args, **kwargs) # вызываем родительский метод get_queryset и сортируем его
+        queryset = queryset.filter(published=True) # фильтруем queryset по признаку публикации
+        return queryset # возвращаем отфильтрованный queryset
+
+
+class BlogDetailView(DetailView): # создаем класс BlogDetailView, который наследуется от DetailView
+    model = Blog
+
+    def get_object(self, queryset=None): # переопределяем метод get_object
+        self.object = super().get_object(queryset) # вызываем родительский метод get_object
+        self.object.views += 1 # увеличиваем количество просмотров на 1
+        self.object.save() # сохраняем изменения
+        return self.object # возвращаем объект
+
+
