185faa502f6857dd736befdef27db15b4b677c7e
annavaleriev <filenko.a@gmail.com>
2024-05-25 18:34:45 +0300

    PEP8

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView, Ar
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path("create/", ArticleCreateView.as_view(), name="create"), # это CreateView
-    path("", ArticleListView.as_view(), name="list"), # это ListView
-    path("view/<int:pk>/", ArticleDetailView.as_view(), name="view"), # это DetailView
-    path("edit/<int:pk>/", ArticleUpdateView.as_view(), name="edit"), # это UpdateView
-    path("delete/<int:pk>/", ArticleDeleteView.as_view(), name="delete"), # это DeleteView
+    path("create/", ArticleCreateView.as_view(), name="create"),  # это CreateView
+    path("", ArticleListView.as_view(), name="list"),  # это ListView
+    path("view/<int:pk>/", ArticleDetailView.as_view(), name="view"),  # это DetailView
+    path("edit/<int:pk>/", ArticleUpdateView.as_view(), name="edit"),  # это UpdateView
+    path("delete/<int:pk>/", ArticleDeleteView.as_view(), name="delete"),  # это DeleteView
 ]
