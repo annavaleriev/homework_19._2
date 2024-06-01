ee719947c45473fbdbeab95be0f759b0b65c33cd
annavaleriev <filenko.a@gmail.com>
2024-06-01 19:36:51 +0300

    Создана ProductForm и реализована кнопка и форма Добавить продукт на главной странице

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.forms import ModelForm
+
+from catalog.models import Product
 
 
 class ContactForm(forms.Form):
     name = forms.CharField()
     phone = forms.CharField()
     message = forms.CharField()
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ["title", "description", "category", "image", "price"]
