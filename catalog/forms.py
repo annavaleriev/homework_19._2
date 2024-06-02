259e25d29dcb38505297624e81c672ff20e64c25
annavaleriev <filenko.a@gmail.com>
2024-06-02 15:15:06 +0300

    Созданы методы, которые проверяют наличие слов в описании м названии

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,10 +1,11 @@
 from django import forms
-from django.forms import ModelForm, BooleanField
+from django.forms import ModelForm
 
 from catalog.models import Product
 
 
 class ContactForm(forms.Form):
+    "Класс формы для обратной связи"
     name = forms.CharField()
     phone = forms.CharField()
     message = forms.CharField()
@@ -22,6 +23,26 @@ class ContactForm(forms.Form):
 
 
 class ProductForm(ModelForm):  # потом вставить StyleFormMixin
+    "Класс формы для создания и редактирования продукта"
+
     class Meta:
         model = Product
         fields = ["title", "description", "category", "image", "price"]
+
+    def clean_title(self):
+        "Метод проверяет наличие запрещенных слов в названии"
+        title = self.cleaned_data["title"]
+        stop_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
+        for word in stop_words:
+            if word in title.lower():
+                raise forms.ValidationError(f"Использовать '{word}' в названии нельзя")
+        return title
+
+    def clean_description(self):
+        "Метод проверяет наличие запрещенных слов в описании"
+        description = self.cleaned_data["description"]
+        stop_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
+        for word in stop_words:
+            if word in description.lower():
+                raise forms.ValidationError(f"Использовать '{word}' в описании нельзя")
+        return description
