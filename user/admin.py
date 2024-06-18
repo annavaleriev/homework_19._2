f66197f25e96f09cac1bd6b455a6320f19187827
annavaleriev <filenko.a@gmail.com>
2024-06-18 18:43:19 +0300

    Создано приложение User

--- user/admin.py
+++ user/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+
+from user.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("email", "phone", "country")
-    # readonly_fields = ("views", )
     search_fields = ("email", "phone")
     ordering = ("email",)
-
-
-admin.site.register(User, UserAdmin)
-
-# admin.site.register(User)
