0258f2f66f5d5f22b7138467dcbf4b5731443abe
annavaleriev <filenko.a@gmail.com>
2024-07-10 12:49:39 +0300

    Добавление owner c null=True

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,10 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 
-NULLABLE = {"null": True, "blank": True}
+from catalog.utils import NULLABLE
+from user.models import User
+
+
 
 
 class Category(models.Model):
@@ -25,6 +28,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
     created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания (записи в БД)")
     updated_at = models.DateField(auto_now=True, verbose_name="Дата последнего изменения (записи в БД)")
+    owner = models.ForeignKey(User, null=True,  on_delete=models.CASCADE, verbose_name="Владелец")
 
     def __str__(self):
         return f"{self.title} {self.category} {self.price}"
